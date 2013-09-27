7d2b549fa506f22d07d2d694f94e920567aa89a7
LostKobrakai <benni@kobrakai.de>
2013-09-27 18:35:00 +0200

    finetuned container selection

--- js/salt.js
+++ js/salt.js
@@ -9,7 +9,7 @@ window.$ = function(selector, ele) {
     '*': 'querySelectorAll'
   }[selector[0]]; // you can treat a string as an array of characters
   // now pass the selector without the key/first character
-  var container = ele ? ele : document;
+  var container = ele instanceof Element ? ele : document;
   var el = (container[matches](selector.slice(1)));
   // if there is one element than return the 0 element
   return ((el.length < 2) ? el[0]: el);
