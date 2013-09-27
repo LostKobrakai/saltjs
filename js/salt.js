4874569ab21d90decb70988240a54c9b1c155782
LostKobrakai <benni@kobrakai.de>
2013-09-27 18:29:40 +0200

    added function to get elements in dom element

--- js/salt.js
+++ js/salt.js
@@ -1,5 +1,5 @@
 /*! Salt.js DOM Selector Lib. By @james2doyle */
-window.$ = function(selector) {
+window.$ = function(selector, ele) {
   // an object containing the matching keys and native get commands
   var matches = {
     '#': 'getElementById',
@@ -9,7 +9,8 @@ window.$ = function(selector) {
     '*': 'querySelectorAll'
   }[selector[0]]; // you can treat a string as an array of characters
   // now pass the selector without the key/first character
-  var el = (document[matches](selector.slice(1)));
+  var container = ele ? ele : document;
+  var el = (container[matches](selector.slice(1)));
   // if there is one element than return the 0 element
   return ((el.length < 2) ? el[0]: el);
 };
